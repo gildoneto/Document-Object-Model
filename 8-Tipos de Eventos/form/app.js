b2000b21bf4d84da3485974d28ceb3277ff4fde4
Gildo Neto <gildo.neto@thoughtworks.com>
2022-08-07 15:56:19 -0300

    focus and type events

--- 8-Tipos de Eventos/form/app.js
+++ 8-Tipos de Eventos/form/app.js
@@ -1,7 +1,24 @@
 // pegar o elemento output <p>
 let output = document.getElementById("output");
 
+// pegar o input textbox
+let nameText = document.querySelector('input[type="text"]');
+
 document.querySelector("form").addEventListener("submit", (e) => {
   output.textContent = `O evento de submit foi efetuado às ${e.timeStamp}`;
   e.preventDefault();
+});
+
+// vamos dar uma olhada em outros tipos de eventos
+nameText.addEventListener("input", () => {
+  output.textContent = `O valor digitado é ${nameText.value}`;
+});
+
+nameText.addEventListener("focus", (e) => {
+  e.target.style.background = "lightblue";
+  e.target.style.color = "black";
+});
+
+nameText.addEventListener("copy", () => {
+  alert(`Você copiou o texto: "${nameText.value}"`);
 });
